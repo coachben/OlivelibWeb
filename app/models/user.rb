4f5eec3d32d90c0207cc89f88f1b9728a82ddee3
Benard Ochang <realtorben2012@gmail.com>
2019-04-24 22:32:36 +0000

    update readme

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
-#Accessing variables ina one to many/many-many relationsship
+# Accessing variables ina one to many/many-many relationsship
 #Select the building: Building.first, or Building.find(3), etc.
 #Select the office: .offices[2]
 #Find the company: .company
